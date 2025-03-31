1a515d97dc02a9d31539270a613590c16f17d4e5
StefKot <96449266+StefKot@users.noreply.github.com>
2025-03-31 23:52:20 +0300

    Added logging

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -19,31 +19,37 @@ public class FilmController {
 
     @GetMapping
     public Collection<Film> getAllFilms() {
+        log.info("Received GET request for /films - retrieving all films");
         return filmService.getAllFilms();
     }
 
     @PostMapping
     public Film create(@Valid @RequestBody Film film) {
+        log.info("Received POST request for /films - creating film: {}", film);
         return filmService.create(film);
     }
 
     @PutMapping
     public Film update(@Valid @RequestBody Film film) {
+        log.info("Received PUT request for /films - updating film: {}", film);
         return filmService.update(film);
     }
 
     @PutMapping("/{id}/like/{userId}")
     public void addLike(@PathVariable int id, @PathVariable int userId) {
+        log.info("Received PUT request for /films/{}/like/{} - adding like", id, userId);
         filmService.addLike(id, userId);
     }
 
     @DeleteMapping("/{id}/like/{userId}")
     public void removeLike(@PathVariable int id, @PathVariable int userId) {
+        log.info("Received DELETE request for /films/{}/like/{} - removing like", id, userId);
         filmService.removeLike(id, userId);
     }
 
     @GetMapping("/popular")
     public List<Film> getPopularFilms(@RequestParam(defaultValue = "10") int count) {
+        log.info("Received GET request for /films/popular?count={} - retrieving popular films", count);
         return filmService.getPopularFilms(count);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -19,36 +19,43 @@ public class UserController {
 
     @GetMapping
     public Collection<User> getAllUsers() {
+        log.info("Received GET request for /users - retrieving all users");
         return userService.getAllUsers();
     }
 
     @PostMapping
     public User create(@Valid @RequestBody User user) {
+        log.info("Received POST request for /users - creating user: {}", user);
         return userService.create(user);
     }
 
     @PutMapping
     public User update(@Valid @RequestBody User user) {
+        log.info("Received PUT request for /users - updating user: {}", user);
         return userService.update(user);
     }
 
     @PutMapping("/{id}/friends/{friendId}")
     public void addFriend(@PathVariable int id, @PathVariable int friendId) {
+        log.info("Received PUT request for /users/{}/friends/{} - adding friend", id, friendId);
         userService.addFriend(id, friendId);
     }
 
     @DeleteMapping("/{id}/friends/{friendId}")
     public void deleteFriend(@PathVariable int id, @PathVariable int friendId) {
+        log.info("Received DELETE request for /users/{}/friends/{} - deleting friend", id, friendId);
         userService.deleteFriend(id, friendId);
-    }
-
-    @GetMapping("/{id}/friends/common/{otherId}")
-    public Set<User> getMutualFriends(@PathVariable int id, @PathVariable int otherId) {
-        return userService.getMutualFriends(id, otherId);
     }
 
     @GetMapping("/{id}/friends")
     public Set<User> getFriends(@PathVariable int id) {
+        log.info("Received GET request for /users/{}/friends - retrieving friends list", id);
         return userService.getFriends(id);
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public Set<User> getMutualFriends(@PathVariable int id, @PathVariable int otherId) {
+        log.info("Received GET request for /users/{}/friends/common/{} - retrieving mutual friends", id, otherId);
+        return userService.getMutualFriends(id, otherId);
     }
 }
